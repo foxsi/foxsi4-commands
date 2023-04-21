2ead707a12ed03515124d1d9dc3271868156e563
Thanasi Pantazides <thanasipantazides@gmail.com>
2023-04-21 17:19:56 -0500

    added command csv parsing, validation, and Python dict generation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # foxsi4-commands
 List of uplink commands for FOXSI-4 onboard systems and associated tools.
+
+## Command structure
+
+Commands sent to the Formatter from the ground have the following structure:
+
+```
+<system ID> <subsystem ID> <R/W bit> <command bits> <arg 1> <arg 2> <EOP>
+```
---
+++ validator.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import json
+from collections import namedtuple
+
+# files we need
+command_file = "command_deck.csv"
+systems_file = "all_systems.json"
+
+asics_per_cdte = 4
+
+# read command deck in
+try:
+    command_data = pd.read_csv(command_file)
+except:
+    raise Exception("couldn't open command_deck.csv")
+
+# read systems list in
+sys_dict = {}
+try:
+    with open(systems_file) as sys_json:
+        sys_dict = json.load(sys_json)
+except:
+    raise Exception("couldn't open systems list")
+
+# clean up systems
+sys_names = [sys["name"].upper() for sys in sys_dict]
+sys_ids = [sys["id"] for sys in sys_dict]
+
+# clean up commands
+head = list(command_data.iloc[0])
+command_systems = [name.upper() for name in head[11:23]]
+
+command_data = command_data[1:]
+command_data.dropna(axis=0, how="all", inplace=True)
+
+# validate systems
+if len(sys_names) != len(set(sys_names)):
+    raise Exception("system names are not unique")
+if len(sys_ids) != len(set(sys_ids)):
+    raise Exception("system ids are not unique")
+
+if len(command_systems) != len(set(command_systems)):
+    raise Exception("system names are not unique")
+
+if set(command_systems) >= set(sys_names):
+    raise Exception("systems in command list are not covered by system list")
+elif set(command_systems) < set(sys_names):
+    print("system list contains the following uncommanded systems: " + str(set(sys_names) - set(command_systems)))
+
+# validate commands
+CommandBitstring = namedtuple("CommandBitstring", "cdte_bits command_bits arg1_bits arg2_bits")
+keys = []
+values = []
+for i, row in command_data.iterrows():
+    cdte_b_str = row[1]
+    rw_b_str = row[2]
+    cmd_b_str = row[3]
+    arg1_b_str = row[6]
+    arg2_b_str = row[7]
+    lookup_instr = row[24]
+    lookup_addr = row[25]
+    lookup_len = row[26]
+    lookup_instr = lookup_instr.replace(" ", "")
+    full_cmd_str = (rw_b_str + cmd_b_str).replace(" ", "")
+    full_cmd_hex = hex(int(full_cmd_str, 2))
+    keys.append(full_cmd_hex)
+    values.append((lookup_instr, lookup_addr, lookup_len))
+
+duplicates = set([key for key in keys if keys.count(key) > 1])
+
+if len(set(keys)) != len(keys):
+    raise Exception("command codes are not unique")
+
+
+# generate files that will generate Dict for each detector
+dict_name = "command_lookup"
+with open("commands.py", "w") as file:
+    file.write(dict_name + " = {} \n")
+    for i, key in enumerate(keys):
+        file.writelines(dict_name + "[" + str(key) + "] = (" + str(values[0]) + "," + str(values[1]) + "," + str(values[2]) + ")\n")
+
+# generate files that will generate C++ std::unordered_map for each detector
+
+# generate .json file
+
+# validate outputed files?
